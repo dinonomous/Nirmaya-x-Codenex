4544d2c1f232afeee88f6410be3518b87c572afb
Clydie-Juls <clyde_marindo@dlsu.edu.ph>
2025-01-02 17:42:42 -0700

    implement tailwind css ui/ux design

--- client/components/pages/heroPage/Page.tsx
+++ client/components/pages/heroPage/Page.tsx
@@ -1,29 +1,78 @@
 import Image from "next/image";
+import { Michroma, Baumans } from "next/font/google";
+
+export const michorma = Michroma({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+export const baumans = Baumans({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export const Hero = () => {
+  const codeTextGradient = "linear-gradient(90deg, #757AD0 0%, #CD89D7 100%)";
   return (
-    <div className="w-full relative">
-      <Image
-        alt="Looper background"
-        src="/images/hero-section-looper-bg.png"
-        fill
-        objectFit="contain"
-      />
-      <div className="flex flex-col gap-4">
-        <h1>people's</h1>
-        <h1>
-          The <span>Code</span> Business
-        </h1>
-        <Image
-          alt="Hero section image"
-          src="/images/hero-section-image.png"
-          fill
-          objectFit="contain"
+    <div style={michorma.style} className="w-full relative flex justify-center items-center">
+      {/* Background */}
+        <img
+          alt="Looper background"
+          src="/images/hero-section-looper-bg.png"
+          className="w-full h-auto object-cover"
         />
-        <h2>
-          Where <span>idea's</span> take lead
+
+      {/* Text */}
+      <div 
+      className="flex flex-col md:gap-4 items-center absolute lg:bottom-1/2 md:bottom-[30%] bottom-[28%]">
+        <div className="relative">
+          <h1
+            style={{
+              background: codeTextGradient,
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent",
+              fontSize: "clamp(26px, 6vw, 96px)" 
+            }}
+            className=" text-[96px] relative left-[18%] "
+          >
+            people&apos;s
+          </h1>
+          <h1 style={{ fontSize: "clamp(26px, 6vw, 96px)" }} className=" bg-transparent leading-[50px]">
+            The <span className=" line-through">Code</span> Business
+          </h1>
+        </div>
+
+        <div className="relative">
+          <Image
+            alt="Hero section image"
+            src="/images/hero-section-image.png"
+            width={686}
+            height={457}
+            className="md:w-[686px] md:h-[457px] w-[340px] h-[220px]"
+            objectFit="contain"
+          />
+          <div
+            style={{
+              background: codeTextGradient
+            }}
+            className="-z-10 blur-3xl md:w-[980px] md:h-[980px] w-[340px] h-[360px] opacity-60  bg-[#18311f] border-8 border-white border-opacity-5 rounded-full absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          ></div>
+        </div>
+        <h2 style={{ fontSize: "clamp(22px, 4vw, 64px)" }} >
+          Where{" "}
+          <span
+            style={{
+              ...baumans.style,
+              background: codeTextGradient,
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent",
+            }}
+          >
+            idea&apos;s
+          </span>{" "}
+          take lead
         </h2>
-      </div>
+      </div> 
     </div>
   );
 };
