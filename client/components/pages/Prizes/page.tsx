21463dbf26a3a69c3e71a1e85d3bc03d4968845f
Sameer <hisameer0026@gmail.com>
2025-01-06 02:21:39 +0530

    feat: completed rewards page

--- client/components/pages/Prizes/page.tsx
+++ client/components/pages/Prizes/page.tsx
@@ -1,119 +1,95 @@
-import React from "react";
-import {
-  FaTrophy,
-  FaMedal,
-  FaSkiingNordic,
-  FaBookOpen,
-  FaUserFriends,
-} from "react-icons/fa";
-import { GiPerson, GiPaintBrush } from "react-icons/gi";
+import Image from "next/image";
+import { Michroma, Baumans, Mynerve, Caveat } from "next/font/google";
+import img1 from "@/public/assets/rewptch.png"
+import img2 from "@/public/assets/rewbld.png"
+import img3 from "@/public/assets/rewintrn.png"
+
+export const michorma = Michroma({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+export const baumans = Baumans({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+export const caveat = Caveat({
+  weight: "400",
+  subsets: ["latin"],
+});
+
 
 export const Prizes = () => {
   return (
     <>
-      <div
-        id="prizes"
-        className="prizesection p-10 bg-slate-100 rounded-3xl m-4 shadow-lg"
-      >
-        <div className="text-center mb-8">
-          <h1 className="text-4xl font-bold text-slate-700">Prize Section</h1>
-          <p className="text-gray-600">
-            Celebrate your achievements with these exciting prizes!
-          </p>
-        </div>
-
-        {/* First Row */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          <div className="bg-white shadow-md rounded-lg p-6 text hover:shadow-lg transition-shadow">
-            <FaTrophy className="text-yellow-500 text-6xl mb-4 " />
-            <h2 className="text-black text-2xl font-semibold">Overall First</h2>
-            <p className=" text-black ">
-              First Overall prize will be given to a project that outshines all
-              other submissions.
-            </p>
-          </div>
-          <div className="bg-white shadow-md rounded-lg p-6  hover:shadow-lg transition-shadow">
-            <FaMedal className="text-yellow-400 text-6xl mb-4 " />
-            <h2 className=" text-black text-2xl font-semibold">
-              Overall Second
-            </h2>
-            <p className=" text-black ">
-              Second Overall prize will be given to the second-best project of
-              the hackathon.
-            </p>
-          </div>
-          <div className="bg-white shadow-md rounded-lg p-6  hover:shadow-lg transition-shadow">
-            <FaSkiingNordic className="text-yellow-500 text-6xl mb-4 " />
-            <h2 className=" text-black text-2xl font-semibold">
-              Overall Third
-            </h2>
-            <p className=" text-black ">
-              The third-best overall project of the hackathon will win some
-              awesome prizes.
-            </p>
-          </div>
-        </div>
-
-        {/* Second Row */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
-          <div className="bg-white shadow-md rounded-lg p-6  hover:shadow-lg transition-shadow">
-            <GiPerson className="text-yellow-500 text-6xl mb-4 " />
-            <h2 className=" text-black text-2xl font-semibold">Best Solo</h2>
-            <p className=" text-black ">
-              Compete solo and stand out to win this prize! Teams are
-              encouraged, but solo entries are rewarded too.
-            </p>
-          </div>
-          <div className="bg-white shadow-md rounded-lg p-6  hover:shadow-lg transition-shadow">
-            <FaTrophy className="text-yellow-500 text-6xl mb-4 " />
-            <h2 className=" text-black text-2xl font-semibold">
-              Best Beginner
-            </h2>
-            <p className=" text-black ">
-              Win this prize if at least 50% of your team are new participants
-              in hackathons.
-            </p>
-          </div>
-          <div className="bg-white shadow-md rounded-lg p-6  hover:shadow-lg transition-shadow">
-            <GiPaintBrush className="text-yellow-500 text-6xl mb-4 " />
-            <h2 className=" text-black text-2xl font-semibold">Best UI/UX</h2>
-            <p className=" text-black ">
-              The project with the most creative and user-friendly design wins
-              this track.
-            </p>
-          </div>
-        </div>
-
-        {/* Third Row */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
-          <div className="bg-white shadow-md rounded-lg p-6  hover:shadow-lg transition-shadow">
-            <h2 className=" text-black text-2xl font-semibold">
-              Best Web App with Qoom
-            </h2>
-            <p className=" text-black ">
-              Utilize Qoom in your project to qualify for this unique category.
-            </p>
-          </div>
-          <div className="bg-white shadow-md rounded-lg p-6  hover:shadow-lg transition-shadow">
-            <FaUserFriends className="text-yellow-500 text-6xl mb-4 " />
-            <h2 className=" text-black text-2xl font-semibold">More Prizes</h2>
-            <p className=" text-black ">
-              Stay tuned for additional exciting prizes to be revealed soon!
-            </p>
-          </div>
-        </div>
+      <div className="text-center pb-24">
+        <span style={michorma.style} className="text-center text-[60px] lg:text-[100px] bg-gradient-to-r from-[#757AD0] to-[#CD89D7] bg-clip-text text-transparent  " >Rewards</span>
       </div>
-      <div
-        id="prizes"
-        className="prizesection p-10 bg-slate-100 rounded-3xl m-4 shadow-lg"
-      >
-        <div className="text-center mb-8">
-          <h1 className="text-4xl font-bold text-slate-700">Non Coding Prizes</h1>
-          <p className="text-gray-600">
-            coming soon
-          </p>
+
+
+
+
+
+      <div className="flex flex-col items-center justify-center gap-8 md:flex-row md:gap-12 lg:gap-16">
+  <div className="text-center">
+    <Image
+      src={img1}
+      alt="Pitch Image"
+      className="w-[300px] h-[320px] md:w-[200px] md:h-[250px] lg:w-[321px] lg:h-[364px]"
+    />
+    <p
+      style={baumans.style}
+      className="text-3xl md:text-5xl lg:text-6xl bg-gradient-to-t from-[#3E5484] to-[#944F9F] bg-clip-text text-transparent mt-4"
+    >
+      Pitch
+    </p>
+  </div>
+
+  <div className="text-center">
+    <Image
+      src={img2}
+      alt="Build Image"
+      className="w-[300px] h-[320px] md:w-[200px] md:h-[250px] lg:w-[321px] lg:h-[364px]"
+    />
+    <p
+      style={baumans.style}
+      className="text-3xl md:text-5xl lg:text-6xl bg-gradient-to-t from-[#3E5484] to-[#944F9F] bg-clip-text text-transparent mt-4"
+    >
+      Build
+    </p>
+  </div>
+
+  <div className="text-center">
+    <Image
+      src={img3}
+      alt="Intern Image"
+      className="w-[300px] h-[320px] md:w-[200px] md:h-[280px] lg:w-[321px] lg:h-[364px]"
+    />
+    <p
+      style={baumans.style}
+      className="text-3xl md:text-5xl lg:text-6xl bg-gradient-to-t from-[#3E5484] to-[#944F9F] bg-clip-text text-transparent mt-4"
+    >
+      Intern
+    </p>
+  </div>
+</div>
+
+
+
+
+      <div className="flex items-center justify-center py-10">
+
+        <div className="w-[1100px] h-[253px] ">
+
+          <p style={baumans.style} className="text-center p-3 md:p-0 lg:p-0 text-[40px] md:text-[44px] lg:text-6xl "> The top teams win an <span className="bg-gradient-to-t from-[#7847CC] to-[#DFA9E7] bg-clip-text text-transparent">internship</span> at <span className="text-[#38FFC77A] ">Niramaya Health Tech</span>, where <span style={caveat.style} className="text-[50px] lg:text-8xl "> innovation </span> meets <span style={caveat.style} className="text-[50px]  lg:text-8xl ">impact</span>.</p>
         </div>
       </div>
     </>
   );
 };
+
+
+
+
+
